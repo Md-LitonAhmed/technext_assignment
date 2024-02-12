3ae34b331807148b25f60b23914eef462acf774c
“liton-ahmed” <litonahmed066@gmail.com>
2024-02-12 23:40:58 +0600

    Profile Page is Created successfully

--- src/ProfileView.jsx
+++ src/ProfileView.jsx
@@ -1,26 +1,26 @@
-import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import Cart from './Cart';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Cart from "./Cart";
 
 export default function ProfileView() {
-    const[person,setPerson]=useState();
-    const {profileId}=useParams()
+  const [person, setPerson] = useState();
+  const { profileId } = useParams();
   useEffect(() => {
     fetch(`https://dummyjson.com/users/${profileId}`)
       .then((res) => res.json())
       .then((data) => {
         setPerson(data);
-       
       })
       .catch((error) => console.log(error));
-      
   }, [profileId]);
-  
+  console.log(person);
+  console.log(profileId);
+
+
   return (
     <>
-    <div>Profile View</div>
-     <Cart person={person} key={person.id}/>
+      <div className="flex justify-center items-center mt-10"><h2>Profile View</h2></div>
+      { person && <Cart person={person} />}
     </>
-    
-  )
+  );
 }
